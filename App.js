3e9bb56165174fdb6d1d4b4aa6ca38d2d2d0db86
Dafin <dafin@users.noreply.github.com>
2017-05-27 11:58:29 +0100

    Initial Commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>My Edits start here right!!!</Text>
+
+      </View>
+    );
+  }
+}
+
+
+
+// 4.styles
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#eee',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
