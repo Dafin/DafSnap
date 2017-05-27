78196f9552d46a4f4c21093e2dd32e297acf4006
Dafin <dafin@users.noreply.github.com>
2017-05-27 15:22:35 +0100

    Camera working

--- App.js
+++ App.js
@@ -1,47 +1,93 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image , Platform} from 'react-native';
+
+import Camera from 'react-native-snap-camera';
+import Emoji from 'react-native-snap-emoji';
+import SnapText from 'react-native-snap-text'
+
+const HEADER_MARGIN = Platform.select({
+    ios:0,
+    android:24 
+  });
 
 export default class App extends React.Component {
+
+  state = {
+    emojiIsVisible: false,
+    textIsVisible: false
+
+  }
+
+
+  changeEmojiState(){
+    this.setState({
+      emojiIsVisible: !this.state.emojiIsVisible
+
+    })
+  }
+
+
+  changeTextState(){
+    this.setState({
+      textIsVisible: !this.state.textIsVisible
+    })
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.header}>
-            <Text style={styles.title}> Hello World</Text>
+      <View style={styles.header}>
+      <Text 
+      style={styles.title} 
+      onPress={this.changeTextState.bind(this)}>
+      SnapDaf
+      </Text>
 
 
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>My Edits start here right!!</Text>
-        </View>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Changes you make will automatically reload.</Text>
+      <Text>Shake your phone to open the developer menu.</Text>
+      <Text>My Edits innit!!</Text>
+      </View>
+
+      <Emoji isVisible={this.emojiIsVisible}>
+      <SnapText isVisible={this.textIsVisible}>
+      <Camera type="front"/> 
+      </SnapText>
+      </Emoji>
 
       </View>
-    );
+      );
+    }
   }
-}
 
 
+  //styles
 
-//4.styles
 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'cyan'
+    },
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'cyan',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  header: {
-    backgroundColor: 'orange',    
-  },
+    header: {
+      backgroundColor: 'orange',
+      alignItems: 'center',
+    },
 
     title: {
-    fontSize : 30,
-    backgroundColor: 'yellow',
-    color: 'white',
-  }
+      fontSize : 30,
+      backgroundColor: 'yellow',
+      color: 'white',
+      marginTop: 20
+    },
+
+    button: {
+      backgroundColor: 'gray'
+    }
 
 
-});
+
+  });
